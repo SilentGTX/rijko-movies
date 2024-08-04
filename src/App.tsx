6aa22c88f556072a2f79f88b5e8e88ec22c3ad1b
budinow <originalhurricane@gmail.com>
2024-08-04 19:12:49 +0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+import VideoPlayer from './components/VideoPlayer';
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <VideoPlayer />
+      </div>
+    </>
+  )
+}
+
+export default App
